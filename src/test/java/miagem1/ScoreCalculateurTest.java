176a65f650395fb8e48172ae006b2c421d932e3d
Franck Silvestre <franck.silvestre@gmail.com>
2019-11-21 10:04:52 +0100

    fix #3 Tests sur ScoreCalculateur

---
+++ src/test/java/miagem1/ScoreCalculateurTest.java
@@ -0,0 +1,45 @@
+package miagem1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by franck on 28/11/2016.
+ */
+public class ScoreCalculateurTest {
+
+    private QuestionAChoixMultiple questionAchoixMultiple;
+    private ScoreCalculateur scoreCalculateur;
+
+    @Before
+    public void setUp() throws Exception {
+        questionAchoixMultiple = new QuestionAChoixMultiple("q1",new ArrayList<Integer>(Arrays.asList(2,3,5)));
+        scoreCalculateur = new ScoreCalculateur();
+    }
+
+    @Test
+    public void testCalculeScore() throws Exception {
+        // given : une liste de réponses fausses
+        List<Integer> lf = new ArrayList<Integer>(Arrays.asList(1,4));
+        // expected : le score vaut zéro
+        assertEquals(0f, scoreCalculateur.calculeScore(lf,questionAchoixMultiple),0.01f);
+
+        // given : une liste de réponses à moitié correcte
+        List<Integer> ltf = new ArrayList<Integer>(Arrays.asList(1,2,3));
+        // expected : le score vaut 2/3 du max
+        assertEquals(66.66f, scoreCalculateur.calculeScore(ltf,questionAchoixMultiple),0.01f);
+
+        // given : une liste de réponses entièrement correcte
+        List<Integer> lt = new ArrayList<Integer>(Arrays.asList(2,3,5));
+        // expected : le score vaut zéro
+        assertEquals(100f, scoreCalculateur.calculeScore(lt,questionAchoixMultiple),0.01f);
+
+    }
+
+}
